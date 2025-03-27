90cb1fd99d9b24c2a9f502ee50f4a8a170259fcf
Cheon, Seongwoo <cjs1399@naver.com>
2025-03-27 14:47:49 +0900

    [CS-swift] programmers_코테는 조금씩

---
+++ swift_cs/swift_cs/250328.swift
@@ -0,0 +1,36 @@
+//
+//  250328.swift
+//  swift_cs
+//
+//  Created by 천성우 on 3/27/25.
+//
+
+import Foundation
+
+
+func programmers_이진_변환_반복하기(_ s:String) -> [Int] {
+    var convertCount = 0
+    var zerosCount = 0
+    var target = s
+    
+    while target != "1" {
+        let zeros = target.filter({ $0 == "0" }).count
+        zerosCount += zeros
+        target = String((target.count - zeros), radix: 2)
+        convertCount += 1
+    }
+    
+    return [convertCount, zerosCount]
+}
+
+
+func programmers_멀리_뛰기(_ n:Int) -> Int {
+    var first = 0
+    var second = 1
+    for i in 0..<n {
+        let temp = first + second
+        first = second
+        second = temp % 1234567
+    }
+    return second
+}
